b5bddceb286e0a37d905ca92cfb576ae05a89fad
Ocione Junior <ocionejunior@gmail.com>
2021-04-19 17:06:20 -0300

    Configuração básica do node e do express

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express()
+
+app.get("/", (request, response) => {
+    return response.json({
+        message: "Olá NLW 05!",
+    })
+})
+
+app.post("/users", (request, response) => {
+    return response.json({message: "Usuario salvo com sucesso"})
+})
+
+app.listen(3333, () => console.log("Server is running on port 3333"))

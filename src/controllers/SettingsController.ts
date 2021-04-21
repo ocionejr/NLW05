3987c7d910c696d3351ff42f39f3ad0cd884cc30
Ocione Junior <ocionejunior@gmail.com>
2021-04-21 17:04:37 -0300

    Migration, entity, repository e controller da tabela settings

---
+++ src/controllers/SettingsController.ts
@@ -0,0 +1,24 @@
+import { getCustomRepository } from 'typeorm';
+import { Request, Response } from "express";
+import { SettingsRepository } from "../repositories/SettingsRepository"
+
+
+class SettingsController {
+
+    async create(request: Request, response: Response) {
+        const { chat, username } = request.body
+
+        const settingsRepository = getCustomRepository(SettingsRepository)
+
+        const settings = settingsRepository.create({
+            chat,
+            username
+        })
+
+        await settingsRepository.save(settings)
+
+        return response.json(settings)
+    }
+}
+
+export { SettingsController }

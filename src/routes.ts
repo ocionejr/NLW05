1e47cf35c95326fd9486bfd1096e0286159d6559
Ocione Junior <ocionejunior@gmail.com>
2021-04-23 19:05:55 -0300

    Estrutura de banco das tabelas message e users

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,19 @@
 import { Router } from "express";
-import { getCustomRepository } from "typeorm";
+import { MessagesController } from "./controllers/MessagesController";
 import { SettingsController } from "./controllers/SettingsController";
-import { SettingsRepository } from "./repositories/SettingsRepository";
+import { UsersController } from "./controllers/UsersController";
 
 const routes = Router()
 
 const settingsController = new SettingsController()
+const usersController = new UsersController()
+const messagesController = new MessagesController()
 
 routes.post("/settings", settingsController.create)
+
+routes.post("/users", usersController.create)
+
+routes.post("/messages", messagesController.create)
+routes.get("/messages/:id", messagesController.showByUser)
 
 export { routes }
